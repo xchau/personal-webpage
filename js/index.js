5d057d23aa0d86ed356d81b2666db8ac28e6f556
Minh Chau <xiu.chau@gmail.com>
2017-05-16 16:03:13 -0700

    work on hover overlay

--- js/index.js
+++ js/index.js
@@ -1,5 +1,32 @@
 'use strict';
 
 (() => {
-  
+  const showOverlayOnHover = () => {
+    const projects = document.querySelectorAll('.project');
+
+    const grid = document.querySelector('#porfolio-grid');
+
+    grid.addEventListener('mouseover', (event) => {
+      const target = event.target;
+
+      if (target.nodeName === 'IMG') {
+        const label = target.dataset.name;
+        const height = target.offsetHeight;
+        const width = target.offsetWidth;
+        const overlay = target.nextElementSibling;
+
+        overlay.classList.remove('hidden');
+        overlay.style.width = `${width}px`;
+        overlay.style.top = `${height / 2}px`;
+
+        console.log(label, height, width);
+        console.log(overlay);
+      }
+
+    })
+  };
+
+  showOverlayOnHover();
+
+
 })();
