59b923b4a30542e7d4eec768a559614611f24778
Minh Chau <xiu.chau@gmail.com>
2017-05-18 17:56:22 -0700

    restrictive overlay

--- js/index.js
+++ js/index.js
@@ -1,48 +1,48 @@
 'use strict';
 
 (() => {
-  const showOverlayOnHover = () => {
-    // const projects = document.querySelectorAll('.project');
-
-    const grid = document.querySelector('#porfolio-grid');
-
-    grid.addEventListener('mouseover', (event) => {
-      const target = event.target;
-
-      if (target.nodeName === 'IMG') {
-        const label = target.dataset.name;
-        const height = target.offsetHeight;
-        const width = target.offsetWidth;
-        const overlay = target.nextElementSibling;
-
-        overlay.classList.remove('hidden');
-        overlay.style.width = `${width}px`;
-        overlay.style.top = `${height / 1.6}px`;
-
-        console.log(label, height, width);
-        console.log(overlay);
-      }
-
-    })
-  };
-
-  // const hideOverlay = () => {
+  // const showOverlayOnHover = () => {
+  //   // const projects = document.querySelectorAll('.project');
+  //
   //   const grid = document.querySelector('#porfolio-grid');
   //
-  //   grid.addEventListener('mouseout', (event) => {
+  //   grid.addEventListener('mouseover', (event) => {
   //     const target = event.target;
   //
   //     if (target.nodeName === 'IMG') {
+  //       const label = target.dataset.name;
+  //       const height = target.offsetHeight;
+  //       const width = target.offsetWidth;
   //       const overlay = target.nextElementSibling;
+  //
+  //       overlay.classList.remove('hidden');
+  //       overlay.style.width = `${width}px`;
+  //       overlay.style.top = `${height / 1.6}px`;
+  //
+  //       console.log(label, height, width);
   //       console.log(overlay);
-  //
-  //       overlay.classList.add('hidden');
-  //
   //     }
-  //   });
-  // }
-
-  // showOverlayOnHover();
+  //
+  //   })
+  // };
+  //
+  // // const hideOverlay = () => {
+  // //   const grid = document.querySelector('#porfolio-grid');
+  // //
+  // //   grid.addEventListener('mouseout', (event) => {
+  // //     const target = event.target;
+  // //
+  // //     if (target.nodeName === 'IMG') {
+  // //       const overlay = target.nextElementSibling;
+  // //       console.log(overlay);
+  // //
+  // //       overlay.classList.add('hidden');
+  // //
+  // //     }
+  // //   });
+  // // }
+  //
+  // // showOverlayOnHover();
 
 
 })();
