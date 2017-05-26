1221f8c8ecdd2b9a1278fc54a178bcadc4dae3e8
Minh Chau <xiu.chau@gmail.com>
2017-05-25 18:19:07 -0700

    refactor morgan

--- server/server.js
+++ server/server.js
@@ -5,13 +5,13 @@ const app = express();
 
 app.disable('x-powered-by');
 
-const morgan = require('morgan');
-
-app.use(morgan('short'));
-
 const projects = require('./routes/projects');
 
 if (process.env.NODE_ENV !== 'production') {
+  const morgan = require('morgan');
+
+  app.use(morgan('short'));
+
   app.use('/', (req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
